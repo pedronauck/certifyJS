e8c6f4f0e09b9c2f7ab2d0ae03cc80cbbbbba989
Pedro Nauck <pedronauck@gmail.com>
2014-04-26 04:16:31 -0300

    add phantomjs to create png certificates

--- app.js
+++ app.js
@@ -1,33 +1,60 @@
 'use strict';
 
 var fs = require('fs'),
+    cp = require('child_process'),
     sh = require('shelljs'),
+    cli = require('cli-color'),
     slug = require('slug'),
     swig = require('swig'),
-    pdf = require('html-pdf'),
-    info = require('./_data.json');
+    path = require('path'),
+    phantomjs = require('phantomjs'),
+    binPath = phantomjs.path,
+    data = require(path.join(__dirname, '_data.json'));
 
-if (!sh.test('-e', './build')) {
-  sh.mkdir('./build');
+var error = cli.red.bold,
+    notice = cli.cyan,
+    done = cli.green;
+
+var dir = {
+  certificates: path.join(__dirname, 'certificates'),
+  libs: path.join(__dirname, 'lib'),
+  templates: path.join(__dirname, 'templates')
+};
+
+if (!sh.test('-e', dir.certificates)) {
+  sh.mkdir(dir.certificates);
 } else {
-  sh.rm('-rf', './build');
-  sh.mkdir('./build');
+  sh.rm('-rf', dir.certificates);
+  sh.mkdir(dir.certificates);
 }
 
-info.participants.forEach(function(participant) {
-  var file = sh.cat('./templates/index.html'),
-      newFile = swig.render(file, {
-        locals: {
-          name: participant.name,
-          course: info.course
-        }
-      }),
-      slugName = slug(participant.name).toLowerCase();
+console.log(notice('Waiting, generating certificates...'));
+data.participants.forEach(function(participant) {
+  var slugName = slug(participant.name).toLowerCase();
+  var phantomJSScript = path.join(dir.libs, 'phantomScript.js');
+  var filePath = path.join(dir.certificates, slugName + '.html');
+  var tpl = fs.readFileSync(path.join(dir.templates, 'index.html'), 'utf-8');
+  var file = swig.render(tpl, {
+    locals: {
+      name: participant.name,
+      course: data.course
+    }
+  });
 
-  newFile = newFile.replace(/href\=\"css/g, 'href="../templates/css');
-  newFile = newFile.replace(/src\=\"images/g, 'src="../templates/images');
+  var args = [
+    phantomJSScript,
+    filePath,
+    path.join(dir.certificates, slugName + '.png'),
+  ];
 
-  fs.writeFile('./build/' + slugName + '.html', newFile, function(err) {
-    console.log('Generate html from ' + participant.name);
+  file = file.replace(/href\=\"css/g, 'href="../templates/css');
+  file = file.replace(/src\=\"images/g, 'src="../templates/images');
+  fs.writeFileSync(filePath, file);
+
+  cp.execFile(binPath, args, function(err, stdout, stderr) {
+    console.log(
+      done('Certificate generated successfully to ' + participant.name)
+    );
+    sh.rm('-f', filePath);
   });
 });
